19f772eadce0aa023b737e6254a255846dd08fcb
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-03-14 19:20:32 +0100

    query to dynamoDB

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,65 @@ import {
 } from "@/components/quoteGenerator/QuoteFeneratorElements";
 import Clouds1 from "@/assets/Clouds1.png";
 import Clouds2 from "@/assets/cloudy-weather.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as queries from '@/src/graphql/queries';
+ import { Amplify } from 'aws-amplify';
+import { GraphQLResult, generateClient } from 'aws-amplify/api';
+import config from '../src/amplifyconfiguration.json';
+Amplify.configure(config);
+
+
+ //Amplify.Amplify.configure(awsconfig);
+const client = generateClient();
+// interface for DynamoDB object
+interface UpdateQuoteInfoData{
+  id:string
+  queryName: string
+  queryGenerated:number
+  createdAt:string
+  updatedAt:string
+}
+
+// type guard for our fetch function
+function isGraphQLResultForQuoteQueryName(response:any):response is GraphQLResult<{
+  quotesQueryName:{
+    items:[UpdateQuoteInfoData]
+  };
+}>{
+  return response.data && response.data.quotesQueryName && response.data.quotesQueryName.items;
+
+}
+
+
+
+
 export default function Home() {
   const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+ // function to fetch our DynamoDB
+ const updateQuoteInfo = async()=>{
+  try {
+
+    const response = await client.graphql({ 
+      query: queries.quoteQueryName,
+      authMode:'iam', // Specify the authentication mode
+      variables:{
+        queryName:"LIVE",
+      },
+    });
+    //Create type guards
+    if(!isGraphQLResultForQuoteQueryName(response)){
+      throw new Error("Response Data is undefined.")
+    }
+    const receiveNumberOfQuetos = response.data.quoteQueryName.items[0].queryGenerated
+    setNumberOfQuotes(receiveNumberOfQuetos);
+  } catch (error) {
+    console.log("error getting quote data",error)
+  }
+ }
+ useEffect(()=>{
+  updateQuoteInfo()
+ })
+ 
   return (
     <div>
       {/* Background */}
